778367c78790cb1badc691eff8a3b8f9a5eed49a
Herbert Wolverson <herberticus@gmail.com>
2021-02-11 14:50:41 -0600

    Add a free-space path loss function.

---
+++ rf-signal-algorithms/src/fspl.rs
@@ -0,0 +1,14 @@
+pub fn free_space_path_loss_db(frequency_mhz: f64, distance_meters: f64) -> f64 {
+    let d = distance_meters / 1000.0;
+    32.44 + (20.0 * frequency_mhz.log10()) + (20.0 * d.log10())
+}
+
+#[cfg(test)]
+mod test {
+    use super::free_space_path_loss_db;
+
+    #[test]
+    fn test_fpl() {
+        assert!(float_cmp::approx_eq!(f64, free_space_path_loss_db(5800.0, 10_000.0), 127.70855987125874, ulps = 2));
+    }
+}
--- rf-signal-algorithms/src/lib.rs
+++ rf-signal-algorithms/src/lib.rs
@@ -2,6 +2,8 @@ mod itwom3;
 pub use itwom3::{
     GroundConductivity, ItwomPointToPoint, PTPClimate, PTPPath, PTPResult, RadioClimate,
 };
+mod fspl;
+pub use fspl::free_space_path_loss_db;
 
 // Include the C library for use in child modules.
 mod c {
