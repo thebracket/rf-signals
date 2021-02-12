0646a2245bd5c3f32a523868a5bd9b89bac2edc1
Herbert Wolverson <herberticus@gmail.com>
2021-02-12 12:25:06 -0600

    Remove EGLI warnings.

--- rf-signal-algorithms/src/egli.rs
+++ rf-signal-algorithms/src/egli.rs
@@ -18,33 +18,33 @@ pub fn egli_path_loss(
     let h2 = rx_height.as_meters();
     let d = distance.as_km();
 
-    let mut Lp50 = f64::NAN;
-    let C1;
-    let C2;
+    let mut lp50;
+    let c1;
+    let c2;
 
     if h1 > 10.0 && h2 > 10.0 {
-        Lp50 = 85.9;
-        C1 = 2.0;
-        C2 = 2.0;
+        lp50 = 85.9;
+        c1 = 2.0;
+        c2 = 2.0;
     } else if h1 > 10.0 {
-        Lp50 = 76.3;
-        C1 = 2.0;
-        C2 = 1.0;
+        lp50 = 76.3;
+        c1 = 2.0;
+        c2 = 1.0;
     } else if h2 > 10.0 {
-        Lp50 = 76.3;
-        C1 = 1.0;
-        C2 = 2.0;
+        lp50 = 76.3;
+        c1 = 1.0;
+        c2 = 2.0;
     } else
     // both antenna heights below 10 metres
     {
-        Lp50 = 66.7;
-        C1 = 1.0;
-        C2 = 1.0;
+        lp50 = 66.7;
+        c1 = 1.0;
+        c2 = 1.0;
     }
 
-    Lp50 += 4.0 * _10log10f(d) + 2.0 * _10log10f(f) - C1 * _10log10f(h1) - C2 * _10log10f(h2);
+    lp50 += 4.0 * _10log10f(d) + 2.0 * _10log10f(f) - c1 * _10log10f(h1) - c2 * _10log10f(h2);
 
-    Lp50
+    lp50
 }
 
 #[inline(always)]
