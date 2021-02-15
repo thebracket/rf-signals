8df6d7eef7e6e4485aeb0be96784a76bc8d3a175
Herbert Wolverson <herberticus@gmail.com>
2021-02-15 11:33:15 -0600

    Update FSPL to use distance/frequency types.

--- rf-signal-algorithms/examples/fspl.rs
+++ rf-signal-algorithms/examples/fspl.rs
@@ -1,4 +1,4 @@
-use rf_signal_algorithms::free_space_path_loss_db;
+use rf_signal_algorithms::{free_space_path_loss_db, Distance, Frequency};
 
 const DISTANCE_METERS: f32 = 1000.0;
 const FREQ_MHZ: f32 = 1500.0;
@@ -6,6 +6,6 @@ const FREQ_MHZ: f32 = 1500.0;
 fn main() {
     println!(
         "Free Space Loss : {}",
-        free_space_path_loss_db(FREQ_MHZ as f64, DISTANCE_METERS as f64)
+        free_space_path_loss_db(Frequency::with_mhz(FREQ_MHZ), Distance::with_meters(DISTANCE_METERS))
     );
 }
--- rf-signal-algorithms/src/fspl.rs
+++ rf-signal-algorithms/src/fspl.rs
@@ -1,18 +1,21 @@
+use super::{Frequency, Distance};
+
 /// Calculate the pure free-space path loss in dB of a signal and distance.
-pub fn free_space_path_loss_db(frequency_mhz: f64, distance_meters: f64) -> f64 {
-    let d = distance_meters / 1000.0;
-    32.44 + (20.0 * frequency_mhz.log10()) + (20.0 * d.log10())
+pub fn free_space_path_loss_db(frequency: Frequency, distance: Distance) -> f64 {
+    let d = distance.as_km();
+    let f = frequency.as_mhz();
+    32.44 + (20.0 * f.log10()) + (20.0 * d.log10())
 }
 
 #[cfg(test)]
 mod test {
-    use super::free_space_path_loss_db;
+    use super::{free_space_path_loss_db, Frequency, Distance};
 
     #[test]
     fn test_fpl() {
         assert!(float_cmp::approx_eq!(
             f64,
-            free_space_path_loss_db(5800.0, 10_000.0),
+            free_space_path_loss_db(Frequency::with_ghz(5.8), Distance::with_kilometers(10)),
             127.70855987125874,
             ulps = 2
         ));
