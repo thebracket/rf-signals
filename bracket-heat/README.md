b86349692e0847341dab1a2c1be6dd9dff3a041b
Herbert Wolverson <herberticus@gmail.com>
2021-02-18 09:43:22 -0600

    Just enough to get a Google Map going.

--- bracket-heat/README.md
+++ bracket-heat/README.md
@@ -5,5 +5,5 @@ An in-development RF heat-mapping system.
 ## Requirements
 
 * This requires nightly Rust, because of Rocket. ```rustup override set nightly``` should do the trick.
-* You need to put your Google Maps API key in `bracket-heat/src/gmap_key.txt`. One line without a `\n` at the end. I didn't want to share my Google account details with you. Sorry.
+* You need to put your Google Maps API key in `bracket-heat/resources/gmap_key.txt`. One line without a `\n` at the end. I didn't want to share my Google account details with you. Sorry.
 
--- bracket-heat/src/main.rs
+++ bracket-heat/src/main.rs
@@ -1,12 +1,29 @@
 #![feature(proc_macro_hygiene, decl_macro)]
-
 #[macro_use] extern crate rocket;
 
+
+// Please save your API key in gmap_key.txt in the `resources` directory.
+const GOOGLE_MAPS_API_KEY : &str = include_str!("../resources/gmap_key.txt");
+const INDEX_HTML : &str = include_str!("../resources/index.html");
+use rocket::Response;
+use rocket::{http::ContentType, http::Status, response::content};
+
+// Yes, it's a static mutable. I don't like it.
+static mut INDEX_FINAL : String = String::new();
+
 #[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+fn index() -> content::Html<&'static str> {
+    content::Html(
+        unsafe { &INDEX_FINAL }
+    )
 }
 
 fn main() {
+    // Do some replace magic to place the correct key and version in the HTML
+    unsafe {
+        INDEX_FINAL = INDEX_HTML
+            .replace("_BANNER_", "Bracket-Heat 0.1")
+            .replace("_GMAPKEY_", GOOGLE_MAPS_API_KEY);
+    }
     rocket::ignite().mount("/", routes![index]).launch();
 }
